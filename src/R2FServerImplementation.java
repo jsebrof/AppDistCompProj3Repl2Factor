54fa047ccec04082cb1513081ad38e0bb492bcae
Jonathan Forbes <jsebrof@gmail.com>
2014-05-13 16:34:51 -0700

    Added our code from Project 2, since it will serve as a nearly complete base for Project 3

---
+++ src/R2FServerImplementation.java
@@ -0,0 +1,65 @@
+import java.rmi.RemoteException;
+import java.util.HashMap;
+
+public class R2FServerImplementation extends java.rmi.server.UnicastRemoteObject implements R2FServerInterface {
+	private static final long serialVersionUID = -3503888069129478963L;
+	private HashMap<String, String> store;
+	private long timestart;
+
+	// Constructor
+	public R2FServerImplementation(HashMap<String, String> the_store, long the_time) throws RemoteException
+	{
+		super();
+		store = the_store;
+		timestart = the_time;
+	}
+
+	public synchronized String Put(String key, String value) throws RemoteException // synchronized means thread safety
+	{
+		String return_string;
+		if (store.containsKey(key))
+		{
+			return_string = "Replaced Value \"" + store.containsKey(key) + "\" for Value \"" + value + "\" for Key \"" + key + "\"";
+		}
+		else
+		{
+			return_string = "Placed Key \"" + key + "\" and Value \"" + value + "\" into the Key/Value store";
+		}
+		store.put(key, value); // place key/value into the Map
+		System.out.println(return_string + " at " + (System.currentTimeMillis()-timestart) + " milliseconds");
+		return return_string;
+	}
+
+	public synchronized String Get(String key) throws RemoteException
+	{
+		String return_string = "Value for Key \"" + key + "\" not found in Key/Value store";
+		if (store.containsKey(key))
+		{
+			return_string = "Value for Key \"" + key + "\" is \"" + store.get(key) + "\"";
+			System.out.println(return_string + " retrieved at " + (System.currentTimeMillis()-timestart) + " milliseconds");
+		}
+		else
+		{
+			System.out.println(return_string + " at " + (System.currentTimeMillis()-timestart) + " milliseconds");
+		}
+		return return_string;
+	}
+
+	public synchronized String Delete(String key) throws RemoteException
+	{
+		String return_string;
+		if (store.containsKey(key))
+		{
+			return_string = "Key \"" + key + "\" Value \"" + store.get(key) + "\" deleted";
+			System.out.println(return_string + " at " + (System.currentTimeMillis()-timestart) + " milliseconds");
+			store.remove(key); // delete key/value from the Map
+		}
+		else
+		{
+			return_string = "No value found in store for Key \"" + key + "\"";
+			System.out.println(return_string + " at " + (System.currentTimeMillis()-timestart) + " milliseconds");
+		}
+		return return_string;
+	}
+}
+
---
+++ src/R2FServerInterface.java
@@ -0,0 +1,7 @@
+public interface R2FServerInterface extends java.rmi.Remote 
+{	
+	public String Put(String key, String value) throws java.rmi.RemoteException;
+	public String Get(String key) throws java.rmi.RemoteException;
+	public String Delete(String key) throws java.rmi.RemoteException;
+}
+
